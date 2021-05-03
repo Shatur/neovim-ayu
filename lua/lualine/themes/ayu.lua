01ae1935c20c502a7a258e569ea2b671c4647f6c
Shatur95 <genaloner@gmail.com>
2021-05-03 18:10:48 +0300

    Use better colors for statusline

--- lua/lualine/themes/ayu.lua
+++ lua/lualine/themes/ayu.lua
@@ -1,27 +1,27 @@
 local colors = require('ayu.colors')
 
-local  ayu = {
+local ayu = {
   visual = {
     a = {fg = colors.bg, bg = colors.accent, gui = 'bold'},
-    b = {fg = colors.fg, bg = colors.panel_shadow}
+    b = {fg = colors.accent, bg = colors.panel_border}
   },
   replace = {
     a = {fg = colors.bg, bg = colors.markup, gui = 'bold'},
-    b = {fg = colors.fg, bg = colors.panel_shadow}
+    b = {fg = colors.markup, bg = colors.panel_border}
   },
   inactive = {
-    c = {fg = colors.fg, bg = colors.bg},
-    a = {fg = colors.fg, bg = colors.panel_shadow, gui = 'bold'},
-    b = {fg = colors.fg, bg = colors.panel_shadow}
+    a = {fg = colors.fg, bg = colors.panel_border, gui = 'bold'},
+    b = {fg = colors.fg, bg = colors.panel_border},
+    c = {fg = colors.fg, bg = colors.panel_border}
   },
   normal = {
-    c = {fg = colors.fg_idle, bg = colors.bg},
     a = {fg = colors.bg, bg = colors.entity, gui = 'bold'},
-    b = {fg = colors.fg, bg = colors.panel_shadow}
+    b = {fg = colors.entity, bg = colors.panel_border},
+    c = {fg = colors.fg, bg = colors.panel_border}
   },
   insert = {
     a = {fg = colors.bg, bg = colors.string, gui = 'bold'},
-    b = {fg = colors.fg, bg = colors.panel_shadow}
+    b = {fg = colors.string, bg = colors.panel_border}
   }
 }
 
