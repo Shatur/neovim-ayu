cd98cfef087ae1133ba238e44eec9dc896057103
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-01-02 13:08:46 +0200

    Revert "Set NotifyBackground"
    This reverts commit 5dd9db767538e62005bb028a88c08ff321248aa2.

--- lua/ayu/init.lua
+++ lua/ayu/init.lua
@@ -220,7 +220,6 @@ local function set_groups()
     LspSignatureActiveParameter = { italic = true },
 
     -- Notify.
-    NotifyBackground = { bg = colors.bg },
     NotifyERROR = { fg = colors.vcs_removed },
     NotifyWARN = { fg = colors.func },
     NotifyINFO = { fg = colors.vcs_added },
